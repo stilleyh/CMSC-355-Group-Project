5c1981029ffe4311de4ef49033187b21625082ae
Adjs135 <vindiolaal@vcu.edu>
2026-04-23 04:26:25 -0400

    stuff. still need to update for html

---
+++ sidenav.js
@@ -0,0 +1,23 @@
+//borrowed code from:https://www.sliderrevolution.com/resources/css-sidebar-menus/
+
+jQuery(function ($) {
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if ($(this).parent().hasClass("active")) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).parent().removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this).next(".sidebar-submenu").slideDown(200);
+            $(this).parent().addClass("active");
+        }
+    });
+
+    $("#close-sidebar").click(function () {
+        $(".page-wrapper").removeClass("toggled");
+    });
+
+    $("#show-sidebar").click(function () {
+        $(".page-wrapper").addClass("toggled");
+    });
+});
